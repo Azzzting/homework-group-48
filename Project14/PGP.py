f697f75c918afa7704d736a5f03fb434619a8438
Azzzting <138744150+Azzzting@users.noreply.github.com>
2023-07-29 11:59:03 +0800

    Update PGP.py

--- Project14/PGP.py
+++ Project14/PGP.py
@@ -4,7 +4,7 @@ from gmssl.sm4 import CryptSM4, SM4_ENCRYPT, SM4_DECRYPT
 
 
 print("------------------------sever加密--------------------------")
-# 由sm2utils生成SM2的密钥
+#生成SM2的密钥
 sk_s = "00B9AB0B828FF68872F21A837FC303668428DEA11DCD1B24429D0C99E24EED83D5"
 pk_s = "B9C9A6E04E9C91F7BA880429273747D7EF5DDEB0BB2FF6317EB00BEF331A83081A6994B8993F3F5D6EADDDB81872266C87C018FB4162F5AF347B483E24620207"
 sk_r = "228a9707053e1b333fb8cb839567a9db4ca1cf5381e9a6a539774e6c3563cdfa"
@@ -14,51 +14,51 @@ print("pk_s:",pk_s,"\npk_r:",pk_r)
 
 # 创建sm4Key和sm4对象
 print("\n协商会话密钥:")
-sm4Key_str = func.random_hex(16)  # 随机产生16位 'a2bc65bca8e41795'
-sm4Key = bytes(sm4Key_str, encoding='utf-8')  # 字符串转bytes
-sm4_crypt = CryptSM4()  # 初始化CryptSM4
-sm4_crypt.set_key(sm4Key, mode=SM4_ENCRYPT)  # 初始化key到CryptSM4 这里传的是SM_DECRYPT
+sm4Key_str = func.random_hex(16)
+sm4Key = bytes(sm4Key_str, encoding='utf-8') 
+sm4_crypt = CryptSM4()
+sm4_crypt.set_key(sm4Key, mode=SM4_ENCRYPT)
 print("Session key：", sm4Key)
 
 # 创建sm2对象,用pk_r对sm4Key进行加密
 print("\n加密会话密钥：")
-sm2_crypt = sm2.CryptSM2(private_key=None, public_key=pk_r)  # 附公钥
-encryptKey = sm2_crypt.encrypt(sm4Key)  # 对sm4Key(bytes) 进行加密,返回bytes
-encryptKey = base64.b64encode(encryptKey)  # bytes 转base64
-print("encryptKey",encryptKey)
+sm2_crypt = sm2.CryptSM2(private_key=None, public_key=pk_r) 
+e_k = sm2_crypt.encrypt(sm4Key)  
+e_k = base64.b64encode(e_k)
+print("encryptKey",e_k)
 
 # sm4对象对数据进行加密
 print("\n加密信息：")
 data ="1234567890"
-data = data.encode("utf-8")  # 列表转字符串转bytes
-encryptData = sm4_crypt.crypt_ecb(data)  # 对数据(bytes)加密
-encryptData = base64.b64encode(encryptData)  # bytes 转base64
-encryptData = encryptData.decode("utf-8")  # 由于转为base64,还是bytes,json不支持,故转为str
-print("encryptData:",encryptData)
+data = data.encode("utf-8") 
+e_data = sm4_crypt.crypt_ecb(data)
+e_data = base64.b64encode(e_data)
+e_data = e_data.decode("utf-8")
+print("encryptData:",e_data)
 
 # 用sk_s对sm4Key签名
 print("\n使用私钥签名：")
-sm2_crypt_s = sm2.CryptSM2(private_key=sk_s, public_key=None)  # 私钥签名, 公钥验证
-random_hex_str = func.random_hex(sm2_crypt.para_len)  # para_len为64, 生成64位随机字符串
-sign = sm2_crypt_s.sign(sm4Key, random_hex_str)  # 签名需要用私钥 , sign为字符串格式
-sign = base64.b64encode(bytes(sign, encoding='utf-8'))  # 字符串转base64
-sign = sign.decode("utf-8")  # bytes转字符串
+sm2_crypt_s = sm2.CryptSM2(private_key=sk_s, public_key=None)
+random_hex_str = func.random_hex(sm2_crypt.para_len)
+sign = sm2_crypt_s.sign(sm4Key, random_hex_str)
+sign = base64.b64encode(bytes(sign, encoding='utf-8'))
+sign = sign.decode("utf-8")
 print("sign:",sign)
 
-print("\n发送(encryptData||encryptKey||sign):")
-result = {"encryptData":encryptData,"encryptKey":encryptKey,"sign":sign}
+print("\n发送(e_data||e_k||sign):")
+result = {"encryptData":e_data,"e_k":e_k,"sign":sign}
 print(result)
 
 
 print("------------------------client解密--------------------------")
 sign = base64.b64decode(result['sign'])
-encryptKey =base64.b64decode(result["encryptKey"])
-encryptData = base64.b64decode(result["encryptData"])
+e_k =base64.b64decode(result["e_k"])
+e_data = base64.b64decode(result["e_data"])
 
 # sm2(附私钥)解密sm4Key
 print("\n解出会话密钥：")
 sm2_crypt_r =sm2.CryptSM2(private_key=sk_r,public_key=None)
-sm4Key_r = sm2_crypt_r.decrypt(encryptKey)  # 解密, 返回bytes
+sm4Key_r = sm2_crypt_r.decrypt(e_k)  # 解密, 返回bytes
 print("Session Key that receiver obtain:",sm4Key_r)
 
 # 公钥签名验证
@@ -70,6 +70,6 @@ assert sm2_crypt_sr.verify(sign,sm4Key_r)
 print("\n用公钥验证签名后解密信息：")
 sm4_crypt_r=CryptSM4()
 sm4_crypt_r.set_key(sm4Key_r,mode=SM4_DECRYPT)
-data_r=sm4_crypt_r.crypt_ecb(encryptData)
+data_r=sm4_crypt_r.crypt_ecb(e_data)
 data_r = data_r.decode('utf-8')
 print("data that receiver obtain:",data_r)
