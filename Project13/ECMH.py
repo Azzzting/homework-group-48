675e31eb26edaf901ca9096eb25919098351c950
Azzzting <138744150+Azzzting@users.noreply.github.com>
2023-07-29 11:17:27 +0800

    Update ECMH.py

--- Project13/ECMH.py
+++ Project13/ECMH.py
@@ -14,24 +14,24 @@ def SHA256(s):
 
 
 def inv(a, n):
-    def ext_gcd(a, b, arr):
+    def gcd(a, b, arr):
         if b == 0:
             arr[0] = 1
             arr[1] = 0
             return a
-        g = ext_gcd(b, a%b, arr)
+        c = gcd(b, a%b, arr)
         t = arr[0]
         arr[0] = arr[1]
         arr[1] = t-int(a/b)*arr[1]
-        return g
+        return c
     arr = [0,1,]
-    gcd = ext_gcd(a, n, arr)
+    gcd = gcd(a, n, arr)
     if gcd == 1:
         return (arr[0]%n+n)%n
     else:
         return -1
 
-def EC_add(p, q):    # 表示无穷远点
+def EC_add(p, q):  
     if p == 0 and q == 0: return 0  # 0 + 0 = 0
     elif p == 0: return q  # 0 + q = q
     elif q == 0: return p  # p + 0 = p
@@ -39,14 +39,14 @@ def EC_add(p, q):    # 表示无穷远点
         if p[0] == q[0]:  
             if (p[1] + q[1]) % P == 0: return 0  # mutually inverse
             elif p[1] == q[1]: return EC_double(p)
-        elif p[0] > q[0]:  # swap if px > qx
+        elif p[0] > q[0]: 
             tmp = p
             p = q
             q = tmp
         r = []
-        slope = (q[1] - p[1]) * inv(q[0] - p[0], P) % P  # 斜率
-        r.append((slope ** 2 - p[0] - q[0]) % P)
-        r.append((slope * (p[0] - r[0])- p[1]) % P)
+        s = (q[1] - p[1]) * inv(q[0] - p[0], P) % P  # 斜率
+        r.append((s ** 2 - p[0] - q[0]) % P)
+        r.append((s * (p[0] - r[0])- p[1]) % P)
         return (r[0], r[1])
 
 def EC_inv(p):
@@ -61,54 +61,51 @@ def EC_sub(p, q):
 
 def EC_double(p):
     r = []
-    slope = (3 * p[0] ** 2 + A) * inv(2 * p[1], P) % P
-    r.append((slope ** 2 - 2 * p[0]) % P)
-    r.append((slope * (p[0] - r[0]) - p[1]) % P)
+    s = (3 * p[0] ** 2 + A) * inv(2 * p[1], P) % P
+    r.append((s ** 2 - 2 * p[0]) % P)
+    r.append((s * (p[0] - r[0]) - p[1]) % P)
     return (r[0], r[1])
 
 
-def msg_to_dot(msg): # 映射到椭圆曲线上的点
+def trans(msg): # 映射到椭圆曲线上的点
     def Legendre(y,p): 
         return pow(y,(p - 1) // 2,p)
     def msg_to_x(m):
-        """消息转换成横坐标"""
-        mdigest = SHA256(m)
+        m = SHA256(m)
         while 1:  # cycle until x belong to QR
-            x = int(mdigest, 16)
+            x = int(m, 16)
             if Legendre(x, P):
                 break
-            mdigest = SHA256(mdigest)
+            m = SHA256(m)
         return x
     def get_y(x):  # y^2 = x^3 + 7 mod P
-        """y ** 2 = x ** 3 + 7
-        已知横坐标，计算纵坐标"""
         right = (x ** 3 + 7) % P
         while 1:
             a = randint(0, P)
             if Legendre(a, P) == -1:
                 break
-        base = int(a + sqrt(a ** 2 - right))
-        expo = (P + 1) // 2
-        y = pow(base, expo, P)
+        b = int(a + sqrt(a ** 2 - right))
+        e = (P + 1) // 2
+        y = pow(b, e, P)
         return y
     x = msg_to_x(msg)
     y = get_y(x)
     return (x, y)
 
 
-def ADD(ecmh, msg):
-    dot = msg_to_dot(msg)
+def add(ecmh, msg):
+    dot = trans(msg)
     tmp = EC_add(ecmh, dot)
     return tmp
 
 def single(msg):
-    return ADD(0, msg)
+    return add(0, msg)
 
 def combine(msg_set):
     ans = single(msg_set[0])
     num = len(msg_set) - 1
     for i in range(num):
-        ans = ADD(ans, msg_set[i+1])
+        ans = add(ans, msg_set[i+1])
     return ans
 
 if __name__=="__main__":
@@ -118,5 +115,4 @@ if __name__=="__main__":
     print('m1签名:', single(m1))
     print('m2签名:', single(m2))
     print('并集为：', combine([single(m1),single(m2)]))
-    print('交集为：', ADD(single(m1), single(m2))
-
+    print('交集为：', add(single(m1), single(m2))
