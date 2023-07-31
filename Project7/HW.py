1883047e90e885f0a54face710f1b673fa7d93ca
Azzzting <138744150+Azzzting@users.noreply.github.com>
2023-07-31 17:47:45 +0800

    Update HW.py

--- Project7/HW.py
+++ Project7/HW.py
@@ -1,80 +1,78 @@
-import random
-from hashlib import sha256
-from pysmx.SM3 import hash_msg
-from gmssl import sm2,func
+import hashlib
+import json
 
-photo = 111
+class HashWire:
+    def __init__(self):
+        self.head = None
 
-def sign(k,d0,photo):
-    seed = str(random.getrandbits(256))
-    s = hash_msg(seed)
+    # 添加证书
+    def add_certificate(self, certificate):
+        if self.head is None:
+            self.head = {
+                "certificate": certificate,
+                "hash": None
+            }
+        else:
+            new_node = {
+                "certificate": certificate,
+                "hash": None
+            }
+            new_node["hash"] = self._calculate_hash(new_node["certificate"], self.head["hash"])
+            self.head = new_node
 
-    if k>0:
-        c = sha256()
-        c.update(s.encode())
-        cc = c.hexdigest()
-    else:
-        cc = s
+    # 获取 HashWire 的根哈希值
+    def get_root_hash(self):
+        if self.head is None:
+            return None
+        return self.head["hash"]
 
-    if d0>0:
-        p = sha256()
-        p.update(s.encode())
-        cp = p.hexdigest()
-    else:
-        cp = s
+    # 计算哈希值
+    def _calculate_hash(self, certificate, previous_hash):
+        sha = hashlib.sha256()
+        if previous_hash is not None:
+            data = certificate.encode() + previous_hash.encode()
+        else:
+            data = certificate.encode()
+        sha.update(data)
+        return sha.hexdigest()
 
-    for i in range(k-1):
-        c = sha256()
-        c.update(cc.encode())
-        cc = c.hexdigest()
-    for i in range(d0-1):
-        p = sha256()
-        p.update(cp.encode())
-        cp = p.hexdigest()
-    private_key = '00B9AB0B828FF68872F21A837FC303668428DEA11DCD1B24429D0C99E24EED83D5'
-    public_key = 'B9C9A6E04E9C91F7BA880429273747D7EF5DDEB0BB2FF6317EB00BEF331A83081A6994B8993F3F5D6EADDDB81872266C87C018FB4162F5AF347B483E24620207'
-    sm2_crypt = sm2.CryptSM2(public_key=public_key, private_key=private_key)
-    random_hex_str = func.random_hex(sm2_crypt.para_len)
-    sign = sm2_crypt.sign(photo, random_hex_str)
-    return cp,cc,sign
+class RangeProof:
+    def __init__(self, values):
+        self.values = values
+        self.hash_wire = HashWire()
+        for value in values:
+            self.hash_wire.add_certificate(str(value))
 
-def verify(d1,c,p,photo,sign):
-    if d1>0:
-        c1 = sha256()
-        c1.update(p.encode())
-        cc1 = c1.hexdigest()
-    else:
-        cc1 = p
-    for i in range(d1-1):
-        c1 = sha256()
-        c1.update(cc1.encode())
-        cc1 = c1.hexdigest()
-    private_key = '00B9AB0B828FF68872F21A837FC303668428DEA11DCD1B24429D0C99E24EED83D5'
-    public_key = 'B9C9A6E04E9C91F7BA880429273747D7EF5DDEB0BB2FF6317EB00BEF331A83081A6994B8993F3F5D6EADDDB81872266C87C018FB4162F5AF347B483E24620207'
-    sm2_crypt = sm2.CryptSM2(public_key=public_key, private_key=private_key)
-    if sm2_crypt.verify(sign, photo) and cc1 ==c:
-        return True
+    # 生成区间证明
+    def generate_proof(self, start_index, end_index):
+        if start_index < 0 or end_index >= len(self.values):
+            return None
+        if start_index > end_index:
+            return None
 
+        proof = []
+        current_node = self.hash_wire.head
+        for i in range(len(self.values)):
+            if i >= start_index and i <= end_index:
+                proof.append(current_node["certificate"])
+            if current_node["hash"] is None:
+                break
+            current_node = {
+                "certificate": current_node["hash"],
+                "hash": None
+            }
+            current_node["hash"] = self.hash_wire._calculate_hash(current_node["certificate"], current_node["hash"])
+            print(current_node)
+        return proof
 
-
-MDP = [312, 303, 233]
-
-def hw(number):
-    if number < MDP[2]:
-        mdp = MDP[2]
-    elif number<MDP[1]:
-        mdp=MDP[1]
-    else:
-        mdp = MDP[0]
-    b = number//100
-    s = number//10-10*b
-    g = number%10
-    B = mdp//100
-    S = mdp//10-10*B
-    G = mdp % 10
-    cpb,ccb,signb = sign(B,B-b,photo)
-    cps,ccs,signs = sign(S,S-s,photo)
-    cpg,ccg,signg = sign(G,G-g,photo)
-    return (verify(b,ccb,cpb,photo,signb) and verify(s,ccs,cps,photo,signs) and verify(g,ccg,cpg,photo,signg))
-
-print(hw(123456))
+# 测试代码
+values = [1, 2, 3, 4, 5]
+range_proof = RangeProof(values)
+a,b=1,3
+c=[]
+proof = range_proof.generate_proof(a, b)
+print("final result:")
+print(proof)
+for i in range(a,b+1):
+    c.append(values[i])
+print(c)
