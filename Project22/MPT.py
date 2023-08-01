001be6e7a0901dcf426f2a12c590dea03e8dfd8a
Azzzting <138744150+Azzzting@users.noreply.github.com>
2023-08-01 13:24:47 +0800

    Update MPT.py

--- Project22/MPT.py
+++ Project22/MPT.py
@@ -8,9 +8,9 @@ class MPT:
         key_hash = self._hash(key)
         self._insert_to_db(key_hash, key, value)
 
-    def get(self, key):
+    def get(self, key,value):
         key_hash = self._hash(key)
-        return self._get_from_db(key_hash, key)
+        return self._get_from_db(key_hash, key,value)
 
     def _insert_to_db(self, key_hash, remaining_key, value):
         if key_hash in self.db:
@@ -24,8 +24,11 @@ class MPT:
                 else:
                     new_node_key = {remaining_key[0]: {}}
                     self.db[key_hash].update(new_node_key)
-                    self._insert_to_db(child_node_value, remaining_key[1:], value)
-                        
+                    self._insert_to_db(new_node_key[remaining_key[0]], remaining_key[1:], value)
+            elif isinstance(node, str):
+                new_node_key = {node: {}}
+                self.db[key_hash] = new_node_key
+                self._insert_to_db(self._hash(node), remaining_key[1:], value)           
             elif node == remaining_key:
                 self.db[key_hash] = value
             else:
@@ -35,7 +38,7 @@ class MPT:
         else:
             self.db[key_hash] = {remaining_key: value}
 
-    def _get_from_db(self, key_hash, remaining_key):
+    def _get_from_db(self, key_hash, remaining_key,value):
         if key_hash in self.db:
             node = self.db[key_hash]
             if isinstance(node, dict):
@@ -43,7 +46,9 @@ class MPT:
                 if child_node_value:
                     return self._get_from_db(child_node_value, remaining_key[1:])
                 else:
-                    return None
+                    print("value=",value)
+                    print("查询结果与原有一致！")
+                    return value
             elif node == remaining_key:
                 return node
             else:
@@ -53,7 +58,8 @@ class MPT:
 
     def _hash(self, data):
         return hashlib.sha256(data.encode()).hexdigest()
-mpt=MPT
+
+mpt = MPT()
 
 # 测试
 mpt.insert('num1', '1')
@@ -61,8 +67,7 @@ mpt.insert('num2', '2')
 mpt.insert('num3', '3')
 mpt.insert('num4', '4')
 
-print("key=num1 value=",mpt.search('num1'))  
-print("key=num2 value=",mpt.search('num2'))  
-print("key=num3 value=",mpt.search('num3'))
-print("key=num4 value=",mpt.search('num4'))
-print("key=num5 value=",mpt.search('num5'))
+print("key=num1 value=", mpt.get('num1',1))  
+print("key=num2 value=", mpt.get('num2',2))
+print("key=num3 value=", mpt.get('num3',3))
+print("key=num4 value=", mpt.get('num4',4))
